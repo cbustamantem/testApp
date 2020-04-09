90e0a77be62e11162c081342a9fb129d86438605
Carlos Bustamante <cbustamante@consultoresinformaticos.com.py>
2020-04-09 01:38:39 -0400

    first commit

---
+++ src/test/java/py/com/testing/testApp/TestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package py.com.testing.testApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
