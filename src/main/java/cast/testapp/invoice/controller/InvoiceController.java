2a4122ccaec5c2a0233d6f909921267531797f2a
Carlos Bustamante <cbustamante@consultoresinformaticos.com.py>
2020-05-16 03:32:12 -0400

    mocking with mockito

--- src/main/java/cast/testapp/invoice/controller/InvoiceController.java
+++ src/main/java/cast/testapp/invoice/controller/InvoiceController.java
@@ -10,5 +10,15 @@ package cast.testapp.invoice.controller;
  * @author cbustamante
  */
 public class InvoiceController {
+    /*
+    BR1	Emitir Facturas			
+    BR1-1	Utilizar un numero de factura unico			
+    BR1-2	La facturacion de productos debe cotejar la disponibilidad de stock			
+    BR1-3	La facturación a credito no debe superar el limite de credito vigente del cliente			
+    BR2	Listar facturas por cliente, contado y credito			
+    BR3	Anular Facturas			
+    BR4	Listar facturas anuladas			
+     */
+    
     
 }
